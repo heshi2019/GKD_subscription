e709b8e62e11a9e5d05521953c7c36c46df5e3f9
heshi2019 <87888533+heshi2019@users.noreply.github.com>
2026-04-06 09:27:46 +0800

    Update subscription.ts
    Signed-off-by: heshi2019 <87888533+heshi2019@users.noreply.github.com>

--- src/subscription.ts
+++ src/subscription.ts
@@ -16,12 +16,12 @@ apps.forEach((appConfig) => {
 });
 
 export default defineGkdSubscription({
-  id: 666,
-  name: 'AIsouler的GKD订阅-已停止维护',
+  id: 6379,
+  name: 'XK的GKD订阅-自用尝试',
   version: 0,
-  author: 'AIsouler',
-  checkUpdateUrl: './AIsouler_gkd.version.json5',
-  supportUri: 'https://github.com/AIsouler/GKD_subscription/issues/new/choose',
+  author: 'XK',
+  checkUpdateUrl: './XK_gkd.version.json5',
+  supportUri: 'https://github.com/heshi2019/GKD_subscription/issues/new/choose',
   categories,
   globalGroups,
   apps: rawApps,
